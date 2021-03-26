c753bda841e615b9a485a4bd60ed31ed1964959e
victordelajarte <29466815+victordelajarte@users.noreply.github.com>
2021-03-26 18:16:52 +0100

    🔨 Improve code

--- src/app/pages/messages-page/messages-page.component.ts
+++ src/app/pages/messages-page/messages-page.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import Message from 'src/app/message.interface';
 import { AuthStateService } from 'src/app/services/state/auth-state.service';
 import { MessagesStateService } from 'src/app/services/state/messages-state.service';
 
@@ -7,19 +9,31 @@ import { MessagesStateService } from 'src/app/services/state/messages-state.serv
   templateUrl: './messages-page.component.html',
   styleUrls: ['./messages-page.component.css'],
 })
-export class MessagesPageComponent implements OnInit {
+export class MessagesPageComponent implements OnInit, OnDestroy {
+  public user: { id: number; username: string };
+  private subscription: Subscription;
+  public messages$: Observable<Message[]>;
+
   constructor(
-    public authState: AuthStateService,
-    public messagesState: MessagesStateService
-  ) {}
+    private authState: AuthStateService,
+    private messagesState: MessagesStateService
+  ) {
+    this.messages$ = this.messagesState.messages$;
+  }
 
   ngOnInit() {
-    this.messagesState.getInitialMessages();
+    this.subscription = this.authState.user$.subscribe((user) => {
+      this.user = user;
+      this.messagesState.getInitialMessages();
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   public async login(): Promise<void> {
     await this.authState.login();
-    this.messagesState.getInitialMessages();
   }
 
   public logout(): void {
